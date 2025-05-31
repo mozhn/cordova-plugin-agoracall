312027f1bbe0b39679ad731952f8297bb2b36737
mozhn <mazlum@tuta.io>
2025-06-01 01:58:21 +0300

    Update AgoraCallManager.m

--- src/ios/AgoraCallManager.m
+++ src/ios/AgoraCallManager.m
@@ -1,176 +1,158 @@
-#import "AgoraCallManager.h"
+#import "AgoraViewController.h"
 
-@implementation AgoraCallManager
+@implementation AgoraViewController
 
-@synthesize accessToken;
-@synthesize channelName;
-@synthesize userId;
-
-+ (instancetype)shareInstance {
-    static AgoraCallManager *shareInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        shareInstance = [[super allocWithZone:NULL] initPrivate];
-    });
-    return shareInstance;
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //[self joinChannel];
+    
+    [self.leaveButton setBackgroundImage:[UIImage imageNamed:@"btn_endcall_normal.png"] forState:UIControlStateNormal];
+    [self.leaveButton setBackgroundImage:[UIImage imageNamed:@"btn_endcall_pressed.png"] forState:UIControlStateHighlighted];
 }
 
-- (instancetype)initPrivate {
-    self = [super init];
-    if (self) {
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+  //[self testLocalPreview];
+   [self startLocalPreviewAndJoin];
+}
+
+- (void)testLocalPreview {
+    // 1) Engine’in initialize edildiğinden emin olun
+    AgoraCallManager *mgr = [AgoraCallManager shareInstance];
+    if (!mgr.agoraKit) {
+        NSLog(@"⚠️ Agora engine henüz init edilmemiş!");
+        return;
     }
-    return self;
-}
 
-+ (id)allocWithZone:(struct _NSZone *)zone {
-    return [AgoraCallManager shareInstance];
-}
-
-- (id)copyWithZone:(NSZone *)zone
-{
-    return self;
-}
-
-- (void)init:(NSString*)appId {
-    self.agoraKit = [AgoraRtcEngineKit sharedEngineWithAppId:appId delegate:self];
-}
-
-- (void)joinChannel {
-    [self requestRequiredPermissions];
-    
-    [self.agoraKit enableAudio];
-    [self.agoraKit enableLocalAudio:true];
-    
-    AgoraRtcChannelMediaOptions *mediaOptions = [AgoraRtcChannelMediaOptions new];
-    mediaOptions.autoSubscribeAudio = true;
-    
-    if([self.channelType isEqualToString:@"video"]) {
-        [self.agoraKit enableVideo];
-        [self.agoraKit enableLocalVideo:true];
-        mediaOptions.autoSubscribeVideo = true;
-    }
-    
-    [self.agoraKit
-        joinChannelByUserAccount:self.userId
-        token:self.accessToken
-     	channelId:self.channelName
-        options:mediaOptions];
-}
-
-- (void)leaveFromChannel {
-    [self.agoraKit disableAudio];
-    [self.agoraKit enableLocalAudio:false];
-    
-    if([self.channelType isEqualToString:@"video"]) {
-        [self.agoraKit disableVideo];
-        [self.agoraKit enableLocalVideo:false];
-    }
-    
-    [self.agoraKit leaveChannel:^(AgoraChannelStats *stat){
-        [[AgoraCall shareInstance] logPluginMessage:@"DISCONNECTED"];
-    }];
-}
-
-- (void)muteMic {
-    [self.agoraKit muteLocalAudioStream:YES];
-}
-
-- (void)unmuteMic {
-    [self.agoraKit muteLocalAudioStream:NO];
-}
-
-- (void)enableCam {
-    [self.agoraKit muteLocalVideoStream:NO];
-    [self.agoraKit enableLocalVideo:true];
-}
-
-- (void)disableCam {
-    [self.agoraKit muteLocalVideoStream:YES];
-    [self.agoraKit enableLocalVideo:false];
-}
-
-- (void)enableSpeakerphone {
-    [self.agoraKit setEnableSpeakerphone:YES];
-}
-
-- (void)disableSpeakerphone {
-    [self.agoraKit setEnableSpeakerphone:NO];
-}
-
-- (void)switchCam {
-    [self.agoraKit switchCamera];
-}
-
-- (void)setLocalVideoCanvas:(AgoraRtcVideoCanvas*)canvas {
-    [self.agoraKit setupLocalVideo:canvas];
-}
-
-- (void)setRemoteVideoCanvas:(AgoraRtcVideoCanvas*)canvas {
-    [self.agoraKit setupRemoteVideo:canvas];
-}
-
-- (void)requestRequiredPermissions {
-    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL grantedCamera)
-    {
+    // 2) İzinleri kontrol edin (asenkron). İzin verildiyse devam:
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL grantedCamera) {
         [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL grantedAudio) {
-            NSLog(@"Microphone and Camera is activated.");
+            if (!grantedCamera || !grantedAudio) {
+                NSLog(@"⚠️ Kamera ya da mikrofon izni yok!");
+                return;
+            }
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // 3) Video motorunu açın
+                [mgr.agoraKit enableVideo];
+                [mgr.agoraKit enableLocalVideo:YES];
+
+                // 4) Local canvas oluşturun
+                AgoraRtcVideoCanvas *localCanvas = [[AgoraRtcVideoCanvas alloc] init];
+                localCanvas.uid = 0; // 0 dersek sunucu bir UID atayacak, preview için yeterli
+                localCanvas.renderMode = AgoraVideoRenderModeHidden;
+                localCanvas.view = self.localView;
+
+                // 5) Canvas ayarlamasını yapın
+                [mgr.agoraKit setupLocalVideo:localCanvas];
+
+                // 6) Preview’ı başlatın
+                [mgr.agoraKit startPreview];
+
+                // Aşağıdaki log, preview başlatıldığında geçmeli:
+                NSLog(@"✅ startPreview çağrıldı – local önizleme başlamalı");
+            });
         }];
     }];
 }
 
-///
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didJoinedOfUid:(NSUInteger)uid elapsed:(NSInteger)elapsed {
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-    AgoraViewController *activeAgoraViewController = (AgoraViewController*)[[window rootViewController] presentedViewController];
-    
-    AgoraRtcVideoCanvas *videoCanvas = [[AgoraRtcVideoCanvas alloc] init];
-    videoCanvas.uid = uid;
-    videoCanvas.renderMode = AgoraVideoRenderModeHidden;
-    videoCanvas.view = [activeAgoraViewController remoteView];
-    [[AgoraCallManager shareInstance] setRemoteVideoCanvas:videoCanvas];
-    
-    [[AgoraCall shareInstance] logPluginMessage:@"PARTICIPANT_CONNECTED"];
+
+- (void)startLocalPreviewAndJoin {
+    [AVCaptureDevice requestAccessForMediaType:AVMediaTypeVideo completionHandler:^(BOOL grantedCamera) {
+        [[AVAudioSession sharedInstance] requestRecordPermission:^(BOOL grantedAudio) {
+            if (!grantedCamera || !grantedAudio) {
+                NSLog(@"⚠️ Kamera veya mikrofon izni reddedildi");
+                return;
+            }
+            // İzinler alındıktan sonra preview ve join işlemini main queue’da yapın
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // 2) Local video canvas’ı ayarlayın
+                AgoraRtcVideoCanvas *videoCanvas = [[AgoraRtcVideoCanvas alloc] init];
+                videoCanvas.uid = [[AgoraCallManager shareInstance].userId integerValue];
+                videoCanvas.renderMode = AgoraVideoRenderModeHidden;
+                videoCanvas.view = self.localView;
+                [[AgoraCallManager shareInstance] setLocalVideoCanvas:videoCanvas];
+                
+                // 3) Agora’dan preview’ı başlatmasını isteyin
+                [[AgoraCallManager shareInstance].agoraKit startPreview];
+                
+                // 4) Agora’ya gerçek join isteğini yapın
+                [[AgoraCallManager shareInstance] joinChannel];
+            });
+        }];
+    }];
 }
 
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didJoinChannel:(NSString*)channel withUid:(NSUInteger)uid elapsed:(NSInteger) elapsed {
-    
-    [[AgoraCall shareInstance] logPluginMessage:@"CONNECTED"];
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.remoteView.frame = self.view.bounds;
+    self.localView.frame = CGRectMake(self.view.bounds.size.width - 110, 30, 100, 170);
 }
 
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didRegisteredLocalUser:(NSString *)userAccount withUid:(NSUInteger)uid {
-    [[AgoraCall shareInstance] logPluginMessage:@"USER_REGISTERED"];
+- (void)joinChannel {
+  self.isMicActive = YES;
+  self.isCamActive = YES;
+  
+  [[AgoraCallManager shareInstance] joinChannel];
+
 }
 
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didLeaveChannelWithStats:(AgoraChannelStats *)stats {
-    [[AgoraCall shareInstance] logPluginMessage:@"DISCONNECTED"];
+- (void)leaveChannel {
+  [[AgoraCallManager shareInstance] leaveFromChannel];
+  
+  for (UIView *view in [self.remoteView subviews]) {
+      [view removeFromSuperview];
+  }
+  for (UIView *view in [self.localView subviews]) {
+      [view removeFromSuperview];
+  }
 }
 
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didOfflineOfUid:(NSUInteger)uid reason:(AgoraUserOfflineReason)reason {
-    UIWindow *window = [[UIApplication sharedApplication] keyWindow];
-    AgoraViewController *activeAgoraViewController = (AgoraViewController*)[[window rootViewController] presentedViewController];
-    for (UIView *view in [activeAgoraViewController.remoteView subviews])
-    {
-        [view removeFromSuperview];
+- (IBAction)leaveButtonClick:(UIButton *)sender {
+    [self.presentingViewController dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)cameraToggleButtonClick:(UIButton *)sender {
+    if (self.isCamActive) {
+        [[AgoraCallManager  shareInstance] disableCam];
+        for (UIView *view in [self.localView subviews])
+        {
+            [view removeFromSuperview];
+        }
+        [self.camButton setBackgroundImage:[UIImage imageNamed:@"btn_camera_toggle_pressed.png"] forState:UIControlStateNormal];
+        self.isCamActive = NO;
+    } else {
+        [[AgoraCallManager shareInstance] enableCam];
+        [self.camButton setBackgroundImage:[UIImage imageNamed:@"btn_camera_toggle_normal.png"] forState:UIControlStateNormal];
+        self.isCamActive = YES;
     }
-    [[AgoraCall shareInstance] logPluginMessage:@"PARTICIPANT_DISCONNECTED"];
+}
+- (IBAction)camSwitchButtonClick:(UIButton *)sender {
+    if (self.isFrontCamActive) {
+        [[AgoraCallManager shareInstance] switchCam];
+        [self.camSwitchButton setBackgroundImage:[UIImage imageNamed:@"btn_switch_camera_pressed.png"] forState:UIControlStateNormal];
+        self.isFrontCamActive = NO;
+    } else {
+        [[AgoraCallManager shareInstance] switchCam];
+        [self.camSwitchButton setBackgroundImage:[UIImage imageNamed:@"btn_switch_camera_normal.png"] forState:UIControlStateNormal];
+        self.isFrontCamActive = YES;
+    }
+}
+- (IBAction)microphoneToggleButtonClick:(UIButton *)sender {
+    if (self.isMicActive) {
+        [[AgoraCallManager shareInstance] muteMic];
+        [self.micButton setBackgroundImage:[UIImage imageNamed:@"btn_mute_normal.png"] forState:UIControlStateNormal];
+        self.isMicActive = NO;
+    } else {
+        [[AgoraCallManager shareInstance] unmuteMic];
+        [self.micButton setBackgroundImage:[UIImage imageNamed:@"btn_unmute_normal.png"] forState:UIControlStateNormal];
+        self.isMicActive = YES;
+    }
 }
 
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didOccurWarning:(AgoraWarningCode)warningCode {
-    NSString *code = [NSString stringWithFormat:@"WARNING_CODE_%ld", (long)warningCode];
 
-    [[AgoraCall shareInstance] logPluginMessage:code];
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+    [self leaveChannel];
 }
-
-- (void)rtcEngine:(AgoraRtcEngineKit *)engine didOccurError:(AgoraErrorCode)errorCode {
-    NSString *code = [NSString stringWithFormat:@"ERROR_CODE_%ld", (long)errorCode];
-    [[AgoraCall shareInstance] logPluginMessage:code];
-}
-
-- (void)rtcEngineRequestToken:(AgoraRtcEngineKit *)engine {
-    [[AgoraCall shareInstance] logPluginMessage:@"TOKEN_EXPIRED"];
-}
-
-
 @end
-
